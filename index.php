801ece686b75b6022422194411ac60a4c0ab77a2
florikz <95587619+florikz@users.noreply.github.com>
2022-11-19 16:14:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
-<?php
-
-echo '<h1> microsoft azure </h1>';
-
-echo '<p> is so cool </p>';
-?>
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Данные формы</title>
+ </head>
+ <body>
+  <form action="/example/handler.php">
+   <p><input name="login"> <input type="password" name="pass"></p>
+   <p><input type="submit"></p>
+  </form>
+ </body>
+</html>
