8b43a5d2b6983f1e4a6eac6f89960d99f818f93d
florikz <95587619+florikz@users.noreply.github.com>
2022-11-19 17:49:12 +0100

    Delete registration.php

--- registration.php
+++
@@ -1,8 +0,0 @@
-$login=$_POST['login'];
-$pas=$_POST['password'];
-$db=mysqli_connect('localhost', 'root', '', 'mybase');
-$query="INSERT INTO users (login, pas) VALUES ('$login', '$pas')";
-$result=mysqli_query($db, $query);
-if ($result)
-header("Location: formreg.php");
-mysqli_close($db);
