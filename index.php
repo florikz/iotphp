222b0c0881db8c9e0a725f36d479b564866c1a85
florikz <95587619+florikz@users.noreply.github.com>
2022-11-19 17:46:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
-<?php header('Content-type: text/html; charset=utf-8'); ?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Страница</title>
-<meta charset="utf-8">
-</head>
-<body>
-<p>Авторизация:</p>
-<form action="authorization.php" method="post">
-  <input name="login" placeholder="Логин">
-  <input type="password" name="password" placeholder="Пароль">
-  <input type="submit" value="Войти">
-</form>
-<p>Регистрация:</p>
-<form action="registration.php" method="post">
-  <input name="login" placeholder="Логин">
-  <input type="password" name="password" placeholder="Пароль">
-  <input type="submit" value="Зарегистрароваться">
-</form>
-</body>
-</html>
+<?php
+if($GET["password"] == "qwerty123"){
+ print("Добро пожаловать!");
+}else{
+ print("Доступ запрещен!");
+}
+?>
