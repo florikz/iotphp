9e6ded2f3296cae556a1f3aee752731e85bf88eb
florikz <95587619+florikz@users.noreply.github.com>
2022-11-20 01:30:36 +0100

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-$uname=$_POST['usr'];
-$pass=$_POST['pwd'];
-
-if(empty($_POST["ok"])){echo "Could not insert data!";}
- else
-{$file=fopen("user.txt","w");
-echo fwrite($file,$uname);
-fclose($file);}
