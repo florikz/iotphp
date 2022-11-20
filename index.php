3ff010b54b082b7a32807b6f073475c7b10d8780
florikz <95587619+florikz@users.noreply.github.com>
2022-11-20 02:35:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
-<?php
-
-<!doctype html>
-<html lang = "en">
-<head>
-    <meta charset="UTF-8">
-    <title>Registration</title>
-</head>
-<body>
-        <form action = "" method = "">
-            <label>Login</lable>
-            <input type = "text" placeholder = "Enter login">
-            <label> Password</label>
-            <input type = "password" placeholder = "Enter your password">
-            <button>Login</button>
-        </form>    
-</body>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
 </html>
-
-?>
