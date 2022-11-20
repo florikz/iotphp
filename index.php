64139c44f74fe87f3f72a4286d14e46b777800cb
florikz <95587619+florikz@users.noreply.github.com>
2022-11-20 03:00:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html lang = "en">
 <head>
     <meta charset="UTF-8">
-    <title>Registration</title>
+    <title>Авторизация и регенерация</title>
 </head>
 <body>
         <link rel = "stylesheet" href="css/main.css">
         <form action = "" method = "">
-            <label>Login</lable>
+            <label>Логин</lable>
             <input type = "text" placeholder = "Enter login">
-            <label> Password</label>
+            <label>Пароль</label>
             <input type = "password" placeholder = "Enter your password">
             <button>Log in</button>
         </form>    
