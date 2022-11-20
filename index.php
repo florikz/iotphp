b57a06bc28741f40e6c0c5b828d8b6e40ce5738b
florikz <95587619+florikz@users.noreply.github.com>
2022-11-20 19:24:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
     <link rel = "stylesheet" href="css/main.css">
 </head>
 <body>
-        <form action = "" method = "">
+        <?php
+            $name = $_POST['name'];
+            $last_name = $_POST['last_name'];
+            $login = $_POST['login'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $password_confirm = $_POST['password_confirm'];    
+            $str = "Name: " .$name."\n"."Last name: ".$last_name;    
+            $f = fopen("qwerty_ggg.txt", 'w') or die("не удалось создать файл");
+            fwrite($f, $login);
+            echo "login $login<br>";
+            ?>
+        <form action = "" method = "post">
             
             <label>Name</label>
             <input type="text" name="name" placeholder="Enter your name">
@@ -21,18 +33,7 @@
             <label>Confirm password</label>
             <input type="password" name="password" placeholder="Enter your password again">
             <button type="submit">Log in</button>
-            <?php
-            $name = $_POST['name'];
-            $last_name = $_POST['last_name'];
-            $login = $_POST['login'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $password_confirm = $_POST['password_confirm'];    
-            $str = "Name: " .$name."\n"."Last name: ".$last_name;    
-            $f = fopen("qwerty_ggg.txt", 'w') or die("не удалось создать файл");
-            fwrite($f, $login);
-            echo "login $login<br>";
-            ?>
+            
         </form> 
 </body>
 </html>
