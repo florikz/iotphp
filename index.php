8aa9d2789ac4beff27106e745f8b81ba76248b48
florikz <95587619+florikz@users.noreply.github.com>
2022-11-20 02:16:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<?php
-  <form method="post" action="" name="signup-form">
-  <div class="form-element">
-  <label>Username</label>
-  <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
-  </div>
-  <div class="form-element">
-  <label>Email</label>
-  <input type="email" name="email" required />
-  </div>
-  <div class="form-element">
-  <label>Password</label>
-  <input type="password" name="password" required />
-  </div>
-  <button type="submit" name="register" value="register">Register</button>
-  </form>
-?>
+<!doctype html>
+<html leng = "en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registration</title>
+    <link rel = "stylesheet" href="reg/css/main.css">
+</head>
+<body>
+
+        <forn action = "" method = "">
+            <label>Login</lable>
+            <input> type = "text">
+            <label> Password</label>
+            <input> type = "password">
+            <button>Log in</button>
+        </form>    
+</body>
+</html>
