624cd72b98d37222015546e0b5c0b3c595a93e19
florikz <95587619+florikz@users.noreply.github.com>
2022-12-22 06:15:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             <input type="password" name="password" placeholder="Enter your password again">
             <button type="submit">Log in</button>
             <?php
-            $v = $_POST['v'];
+            $v = $_GET['v'];
             $name = $_POST['name'];
             $last_name = $_POST['last_name'];
             $login = $_POST['login'];
